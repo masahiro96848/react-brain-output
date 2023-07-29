bb515c5e7f92cb40a94443bf380c16ceb8d99acd
masahiro96848 <feetwcpic9436@gmail.com>
2023-07-29 23:32:48 +0900

    Todoを追加、削除機能を実装

--- src/components/templates/index.tsx
+++ src/components/templates/index.tsx
@@ -1,18 +1,57 @@
-import React from 'react'
+import { useState, FormEvent, ChangeEvent } from 'react'
+import { v4 as uuidv4 } from 'uuid'
 import { RiDeleteBinLine } from 'react-icons/ri'
 import styles from './styles.module.css'
 
+type Todo = {
+  id: string
+  title: string
+}
+
 export const TodoTemplate = () => {
+  const sampleTodo: Todo[] = [
+    { id: '1', title: '野球' },
+    { id: '2', title: 'サッカー' },
+    { id: '3', title: 'バスケットボール' },
+  ]
+
+  const [todo, setTodo] = useState('')
+  const [todoList, setTodoList] = useState<Todo[]>(sampleTodo)
+
+  const handleAddTodo = (e: ChangeEvent<HTMLInputElement>) => {
+    setTodo(e.target.value)
+  }
+
+  const handleSubmitTodo = (e: FormEvent) => {
+    e.preventDefault()
+    if (todo === '') return
+    const newTodo: Todo = {
+      id: uuidv4(),
+      title: todo,
+    }
+    setTodoList([...todoList, newTodo])
+    setTodo('')
+  }
+
+  const handleDeleteTodo = (id: string) => {
+    const todo = todoList.filter((item) => item.id !== id)
+    setTodoList(todo)
+  }
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Todo List</h1>
       {/* Todo追加エリア */}
       <section className={styles.common}>
-        <input
-          type="text"
-          className={styles.input}
-          placeholder="Todoを入力してください"
-        />
+        <form onSubmit={handleSubmitTodo}>
+          <input
+            type="text"
+            className={styles.input}
+            value={todo}
+            placeholder="Todoを入力してください"
+            onChange={handleAddTodo}
+          />
+        </form>
       </section>
       {/* Todo検索エリア */}
       <section className={styles.common}>
@@ -26,18 +65,17 @@ export const TodoTemplate = () => {
       <section className={styles.common}>
         <div>
           <ul className={styles.list}>
-            <li className={styles.todo}>
-              <span className={styles.task}>テスト01</span>
-              <div className={styles.icons}>
-                <RiDeleteBinLine size={25} />
-              </div>
-            </li>
-            <li className={styles.todo}>
-              <span className={styles.task}>テスト02</span>
-              <div className={styles.icons}>
-                <RiDeleteBinLine size={25} />
-              </div>
-            </li>
+            {todoList.map((item) => (
+              <li key={item.id} className={styles.todo}>
+                <span className={styles.task}>{item.title}</span>
+                <div
+                  className={styles.icons}
+                  onClick={() => handleDeleteTodo(item.id)}
+                >
+                  <RiDeleteBinLine size={25} />
+                </div>
+              </li>
+            ))}
           </ul>
         </div>
       </section>
