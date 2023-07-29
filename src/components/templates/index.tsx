816635a4598663db5edb2f5f8b5e9772b52e3d57
masahiro96848 <feetwcpic9436@gmail.com>
2023-07-29 21:42:26 +0900

    Todoの追加、検索、リストのレイアウトを作成

--- src/components/templates/index.tsx
+++ src/components/templates/index.tsx
@@ -1,16 +1,46 @@
 import React from 'react'
+import { RiDeleteBinLine } from 'react-icons/ri'
 import styles from './styles.module.css'
 
 export const TodoTemplate = () => {
   return (
     <div className={styles.container}>
-      <h1>Todo List</h1>
+      <h1 className={styles.title}>Todo List</h1>
       {/* Todo追加エリア */}
-      <section></section>
+      <section className={styles.common}>
+        <input
+          type="text"
+          className={styles.input}
+          placeholder="Todoを入力してください"
+        />
+      </section>
       {/* Todo検索エリア */}
-      <section></section>
+      <section className={styles.common}>
+        <input
+          type="text"
+          className={styles.input}
+          placeholder="キーワードを入力してください"
+        />
+      </section>
       {/* Todoリスト表示エリア */}
-      <section></section>
+      <section className={styles.common}>
+        <div>
+          <ul className={styles.list}>
+            <li className={styles.todo}>
+              <span className={styles.task}>テスト01</span>
+              <div className={styles.icons}>
+                <RiDeleteBinLine size={25} />
+              </div>
+            </li>
+            <li className={styles.todo}>
+              <span className={styles.task}>テスト02</span>
+              <div className={styles.icons}>
+                <RiDeleteBinLine size={25} />
+              </div>
+            </li>
+          </ul>
+        </div>
+      </section>
     </div>
   )
 }
