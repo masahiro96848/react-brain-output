fff4bea4b20a39bd1559e8c585914e0763325165
masahiro96848 <feetwcpic9436@gmail.com>
2023-07-30 11:40:01 +0900

    vitestの環境を構築

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
+import type { UserConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import type { InlineConfig } from 'vitest'
+
+interface VitestConfigExport extends UserConfig {
+  test: InlineConfig
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/react-first-output',
+  test: {
+    environment: 'jsdom',
+  },
+} as VitestConfigExport)
